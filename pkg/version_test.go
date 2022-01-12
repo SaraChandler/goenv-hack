114d153d951bcbfed8449c1d9348285c1cf193f7
SaraChandler <15317424+SaraChandler@users.noreply.github.com>
2022-01-12 15:18:56 -0800

    compare version test

--- pkg/version_test.go
+++ pkg/version_test.go
@@ -16,3 +16,14 @@ func TestValidateVersion(t *testing.T) {
 	errInvalid := ValidateVersion(invalidVersion)
 	assert.NotNil(t, errInvalid, "expect an error for invalid version")
 }
+
+func TestCompareVersions(t *testing.T) {
+	versionA := "1.16.5"
+	versionB := "1.16.5"
+
+	compareSame, err := CompareVersions(versionA, versionB)
+	expectSame := 0
+	assert.Equal(t, compareSame, expectSame, "expect no error for valid version")
+	assert.Nil(t, err, "expect no error for compared versions")
+
+}

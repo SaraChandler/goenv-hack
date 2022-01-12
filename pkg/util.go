c8f619ba8c83ee32da269759b1f42ff25f4fb554
Kyle Penfound <kpenfound11@gmail.com>
2022-01-12 13:07:23 -0500

    create download function

--- pkg/util.go
+++ pkg/util.go
@@ -1,7 +1,53 @@
 package pkg
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/hashicorp/errwrap"
+)
+
 // Simple download function to download a given url to a path
 func Download(url string, path string) error {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return errwrap.Wrapf("error in Download: {{err}}", err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("expected 200, got %d", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+
+	// Create path and file
+	err = os.MkdirAll(path, 0755)
+	if err != nil {
+		return err
+	}
+
+	filename := filepath.Base(url)
+	fullFilename := filepath.Join(path, filename)
+	file, err := os.Create(fullFilename)
+	if err != nil {
+		return err
+	}
+
+	size, err := io.Copy(file, resp.Body)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	fmt.Printf("Downloaded %s, size: %d\n", fullFilename, size)
+
 	return nil
 }
 

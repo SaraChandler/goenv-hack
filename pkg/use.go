1e6db41d04bf0ecf1ccba64a64a810de6b931ba9
Kyle Penfound <kpenfound11@gmail.com>
2022-01-12 15:35:16 -0500

    implement some of use and list

--- pkg/use.go
+++ pkg/use.go
@@ -1,5 +1,32 @@
 package pkg
 
+import (
+	"os"
+	"path/filepath"
+)
+
 func Use(version string) error {
-	return nil
+	installed, err := ListInstalledVersions()
+	if err != nil {
+		return err
+	}
+
+	isInstalled, err := isVersionInList(version, installed)
+	if err != nil {
+		return err
+	}
+
+	if !isInstalled {
+		err = Install(version)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Remove link
+	_ = os.Remove(InstallBin)
+
+	// Create new symlink
+	installedBin := filepath.Join(InstallVersionsDir, version, "bin")
+	return os.Symlink(installedBin, InstallBin)
 }

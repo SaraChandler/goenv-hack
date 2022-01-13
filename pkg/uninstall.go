445f6cd686e496cd40582749742490eb21a0e514
SaraChandler <15317424+SaraChandler@users.noreply.github.com>
2022-01-13 15:11:53 -0800

    add uninstall functions

--- pkg/uninstall.go
+++ pkg/uninstall.go
@@ -2,12 +2,34 @@ package pkg
 
 import (
 	"fmt"
-	// "io/ioutil"
-	// "os"
-	// "strings"
+  "path/filepath"
+  "runtime"
+	"os"
 )
 
 func UninstallVersion(version string) error {
-  fmt.Println("you've reached uninstall version func")
+  downloadPath, versionPath := getInstallLocation(version)
+  err := os.RemoveAll(downloadPath)
+  if err != nil {
+    return err
+  }
+
+  err = os.RemoveAll(versionPath)
+  if err != nil {
+    return err
+  }
+  fmt.Printf("Version %v is deleted\n", version)
   return nil
+}
+
+func getInstallLocation(version string) (string, string){
+  os := runtime.GOOS
+  arch := runtime.GOARCH
+  downloadFile := fmt.Sprintf("go%s.%s-%s.tar.gz", version, os, arch)
+
+  downloadPath := filepath.Join(getPath(DownloadDir), downloadFile)
+
+  versionPath := filepath.Join(getPath(InstallVersionsDir), version)
+
+  return downloadPath, versionPath
 }

cb33fc73e909c169c09c045a99ade2a25711ccf3
SaraChandler <15317424+SaraChandler@users.noreply.github.com>
2022-01-14 06:35:40 -0800

    fix indentation

--- cmd/uninstall.go
+++ cmd/uninstall.go
@@ -33,20 +33,20 @@ func (i *UninstallCommand) Run(args []string) int {
     return 1
   }
 
-	installedVersions, err := pkg.ListInstalledVersions()
-	if err != nil {
-		fmt.Println(err)
-		return 1
-	}
+  installedVersions, err := pkg.ListInstalledVersions()
+  if err != nil {
+  	fmt.Println(err)
+  	return 1
+  }
 
-	activeVersion, err := pkg.GetActiveVersion()
-	if err != nil {
-		fmt.Println(err)
-		return 1
-	}
+  activeVersion, err := pkg.GetActiveVersion()
+  if err != nil {
+  	fmt.Println(err)
+  	return 1
+  }
 
-	for _, v := range installedVersions {
-		if v == version {
+  for _, v := range installedVersions {
+  	if v == version {
       if activeVersion != version {
         err := pkg.UninstallVersion(version)
         if err != nil {
@@ -58,8 +58,8 @@ func (i *UninstallCommand) Run(args []string) int {
         fmt.Println("Requested version to uninstall is currently active. Deactivate to uninstall.\n")
         return 1
       }
-		}
-	}
+  	}
+  }
   fmt.Printf("Version %v is not installed\n", version)
-	return 0
+  return 0
 }
